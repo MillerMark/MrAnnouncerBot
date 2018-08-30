a0c3d83bb102c289932a5376eaa6aea80deeb14c
Mark Miller <coderush@gmail.com>
2018-08-30 09:40:22 -0600

    Added docking and undocking to the spaceship.

--- OverlayManager/GameDev/Source/Main.js
+++ OverlayManager/GameDev/Source/Main.js
@@ -1,13 +1,15 @@
 ﻿
 function updateScreen() {
-  myContext.clearRect(0, 0, 1920, 1080);
+  myContext.clearRect(0, 0, 1820, 980);
   myRocket.updatePosition();
-  myRocket.bounce(0, 0, 1920, 1080);
+  myRocket.bounce(0, 0, 1820, 980);
   myRocket.draw(myContext);
+  spinningCoin.draw(myContext, 0, 0);
 }
 
 function handleKeyDown(evt) {
   const Key_C = 67;
+  const Key_D = 68;
   const Key_Up = 38;
   const Key_Right = 39;
   const Key_Left = 37;
@@ -15,7 +17,14 @@ function handleKeyDown(evt) {
 
   evt = evt || window.event;
   if (evt.keyCode == 13) {
-    myRocket.changeVelocity(0, 0);
+    if (!started || myRocket.isDocked) {
+      started = true;
+      myRocket.launch();
+    }
+    else if (myRocket.enginesRetracted)
+      myRocket.extendEngines();
+    else
+      myRocket.retractEngines();
     return false;
   }
   else if (evt.keyCode == Key_Up) {
@@ -34,6 +43,9 @@ function handleKeyDown(evt) {
     myRocket.fireRightThruster();
     return false;
   }
+  else if (evt.keyCode == Key_D) {
+    myRocket.dock();
+  }
   else if (evt.keyCode == Key_C) {
     if (myRocket.chuteDeployed)
       myRocket.retractChutes();
@@ -43,8 +55,13 @@ function handleKeyDown(evt) {
   }
 }
 
+spinningCoin = new Part("Spinning Coin/SpinningCoin", 165, PartStyle.Loop, 960 - 32, 640 - 32, 5);
+
 document.onkeydown = handleKeyDown;
 var myCanvas = document.getElementById("myCanvas");
 var myContext = myCanvas.getContext("2d");
 var myRocket = new Rocket(0, 0);
+var started = false;
+myRocket.x = 0;
+myRocket.y = 0;
 setInterval(updateScreen, 5);
--- OverlayManager/GameDev/Source/Rocket.js
+++ OverlayManager/GameDev/Source/Rocket.js
@@ -23,45 +23,44 @@ const RightFlameY = 181 - CodeRushedPosY;
 const RightFlameJiggleX = 1;
 const RightFlameJiggleY = 0;
 
-
-const BottomLeftEngineX = 132 - CodeRushedPosX;
+const BottomEngineAdjust = 40;
+const BottomLeftEngineX = 132 - BottomEngineAdjust - CodeRushedPosX;
 const BottomLeftEngineY = 210 - CodeRushedPosY;
 const BottomLeftEngineRetractionOffsetX = 0;
 const BottomLeftEngineRetractionOffsetY = -46;
 
 
-const BottomRightEngineX = 279 - CodeRushedPosX;
+const BottomRightEngineX = 279 + BottomEngineAdjust - CodeRushedPosX;
 const BottomRightEngineY = 210 - CodeRushedPosY;
 const BottomRightEngineRetractionOffsetX = 0;
 const BottomRightEngineRetractionOffsetY = -46;
 
 
-const BottomLeftFlameX = 137 - CodeRushedPosX;
+const BottomLeftFlameX = 137 - BottomEngineAdjust - CodeRushedPosX;
 const BottomLeftFlameY = 228 - CodeRushedPosY;
 const BottomLeftFlameJiggleX = 0;
 const BottomLeftFlameJiggleY = 2;
 
-const MiniBottomLeftFlameX = 149 - CodeRushedPosX;
+const MiniBottomLeftFlameX = 149 - BottomEngineAdjust - CodeRushedPosX;
 const MiniBottomLeftFlameY = 245 - CodeRushedPosY;
 const MiniBottomLeftFlameJiggleX = 0;
 const MiniBottomLeftFlameJiggleY = 2;
 
 
 
-const BottomRightFlameX = 284 - CodeRushedPosX;
+const BottomRightFlameX = 284 + BottomEngineAdjust - CodeRushedPosX;
 const BottomRightFlameY = 228 - CodeRushedPosY;
 const BottomRightFlameJiggleX = 0;
 const BottomRightFlameJiggleY = 2;
 
-const MiniBottomRightFlameX = 296 - CodeRushedPosX;
+const MiniBottomRightFlameX = 296 + BottomEngineAdjust - CodeRushedPosX;
 const MiniBottomRightFlameY = 245 - CodeRushedPosY;
 const MiniBottomRightFlameJiggleX = 0;
 const MiniBottomRightFlameJiggleY = 1;
 
-
-
 const ChuteExtendX = 144 - CodeRushedPosX;
 const ChuteExtendY = 6 - CodeRushedPosY;
+const ChuteMaxVelocity = 2; // m/s
 
 class Rocket {
   constructor(x, y) {
@@ -109,6 +108,7 @@ class Rocket {
     this.chuteRetracting = false;
 
     this.changeVelocity(0, 0);
+    this.isDocked = true;
 
     this.worldAcceleration = Gravity.earth;
     this.stopped = true;
@@ -117,9 +117,13 @@ class Rocket {
 
     this.width = 298;   // Widh
     this.height = 52;
+    this.enginesRetracted = true;
   }
 
+  
   deployChute() {
+    if (this.isDocked)
+      return;
     this.logState('Deploying chute...');
     this.chuteDeployed = true;
     this.chuteSailsAreFull = false;
@@ -130,7 +134,8 @@ class Rocket {
     var secondsPassed = (currentTime - this.timeStart) / 1000;
     var newVelocity = Physics.getFinalVelocity(secondsPassed, this.velocityY, this.getVerticalAcceleration());
 
-    newVelocity /= 4;
+    if (newVelocity > ChuteMaxVelocity)
+      newVelocity > ChuteMaxVelocity;
 
     this.changeVelocity(this.velocityX, newVelocity);
     this.chuteSailsAreFull = true;
@@ -146,8 +151,19 @@ class Rocket {
   }
 
   getVerticalAcceleration() {
+    if (this.isDocked)
+      return 0;
+
+    if (this.docking)
+      if (this.dockedTop)
+        return 0;
+      else 
+        return -this.worldAcceleration / 2;
+
     if (this.chuteSailsAreFull)
       return 0;
+    if (this.retractingEngines || this.enginesRetracted)
+      return this.worldAcceleration;
     var now = new Date();
     //console.log('now: ' + now + ', hoverThrusterRestoreTime: ' + this.hoverThrusterRestoreTime + ', mainThrusterOfftime: ' + this.mainThrusterOfftime);
     if (this.hoverThrusterRestoreTime > now)
@@ -160,8 +176,16 @@ class Rocket {
   }
 
   getHorizontalAcceleration() {
-    var now = new Date();
+    if (this.isDocked)
+      return 0;
     const horizontalThrust = 5;
+    if (this.docking)
+      if (this.dockedLeft)
+        return 0;
+      else 
+        return -horizontalThrust;
+
+    var now = new Date();
     var acceleration = 0;
     if (this.leftThrusterOfftime > now)
       acceleration += horizontalThrust;
@@ -173,11 +197,15 @@ class Rocket {
   bounce(left, top, right, bottom) {
     var now = new Date();
     var secondsPassed = (now - this.timeStart) / 1000;
-    const horizontalBounceDecay = 0.94;
-    const verticalBounceDecay = 0.90;
+    var horizontalBounceDecay = 0.74;
+    var verticalBounceDecay = 0.60;
+    if (this.docking) {
+      horizontalBounceDecay /= 4;
+      verticalBounceDecay /= 4;
+    }
 
-    var velocityX = Physics.getFinalVelocity(secondsPassed, this.velocityX, this.getHorizontalAcceleration()) * horizontalBounceDecay;
-    var velocityY = Physics.getFinalVelocity(secondsPassed, this.velocityY, this.getVerticalAcceleration()) * verticalBounceDecay;
+    var velocityX = Physics.getFinalVelocity(secondsPassed, this.velocityX, this.getHorizontalAcceleration());
+    var velocityY = Physics.getFinalVelocity(secondsPassed, this.velocityY, this.getVerticalAcceleration());
 
     var hitLeftWall = velocityX < 0 && this.x < left;
     var hitRightWall = velocityX > 0 && this.x + this.width > right;
@@ -185,20 +213,44 @@ class Rocket {
     var hitTopWall = velocityY < 0 && this.y < top;
     var hitBottomWall = velocityY > 0 && this.y + this.height > bottom;
 
-    if (hitBottomWall)
+    if (hitBottomWall && myRocket.chuteDeployed)
       this.retractChutes();
 
+    var newVelocityX = this.velocityX;
+    var newVelocityY = this.velocityY;
     if (hitLeftWall || hitRightWall)
-      this.changeVelocity(-velocityX, velocityY);
-    else if (hitTopWall || hitBottomWall)
-      this.changeVelocity(velocityX, -velocityY);
-    else
-      return;
+      newVelocityX = -velocityX * horizontalBounceDecay;
+    if (hitTopWall || hitBottomWall)
+      newVelocityY = -velocityY * verticalBounceDecay;
+
+    if (this.docking && hitTopWall) {
+      newVelocityY = 0;
+      this.y = 0;
+      this.dockedTop = true;
+    }
+
+    if (this.docking && hitLeftWall) {
+      newVelocityX = 0;
+      this.x = 0;
+      this.dockedLeft = true;
+    }
+
+    if (this.docking && this.dockedLeft && this.dockedTop) {
+      this.docking = false;
+      this.dockedLeft = false;
+      this.dockedTop = false;
+      this.isDocked = true;
+      this.retractEngines();
+    }
+
+
+    if (hitLeftWall || hitRightWall || hitTopWall || hitBottomWall)
+      this.changeVelocity(newVelocityX, newVelocityY);
   }
 
   getNowPlus(seconds) {
     var t = new Date();
-    t.setSeconds(t.getSeconds() + seconds);
+    t.setMilliseconds(t.getMilliseconds() + seconds * 1000);
     return t;
   }
 
@@ -210,21 +262,86 @@ class Rocket {
     this.changeVelocity(velocityX, velocityY);
   }
 
+  dock() {
+    this.logState('Docking...');
+    if (this.enginesRetracted)
+      this.extendEngines();
+    this.changingDirection();
+    this.docking = true;
+  }
+
+  extendEngines() {
+    this.changingDirection();
+    this.logState('Extending engines...');
+    this.extendingEngines = true;
+    var now = new Date();
+    this.extensionStartTime = now;
+    this.extensionEndTime = this.getNowPlus(0.4);
+  }
+
+  retractEngines() {
+    this.changingDirection();
+    this.logState('Retracting engines...');
+    this.retractingEngines = true;
+    var now = new Date();
+    this.retractionStartTime = now;
+    this.retractionEndTime = this.getNowPlus(0.4);
+    this.killInProgressVerticalDrop();
+    this.killInProgressVerticalThrust();
+    this.killInProgressHorizontalThrust();
+  }
+
+  killInProgressHorizontalThrust() {
+    var now = new Date();
+    if (this.leftThrusterOfftime > now) {
+      this.leftThrusterOfftime = now;
+      this.wasFiringLeftThruster = false;
+    }
+    if (this.rightThrusterOfftime > now) {
+      this.rightThrusterOfftime = now;
+      this.wasFiringRightThruster = false;
+    }
+  }
+
+  killInProgressVerticalThrust() {
+    var now = new Date();
+    if (this.mainThrusterOfftime > now) {
+      this.mainThrusterOfftime = now;
+      this.wasFiringMainThrusters = false;
+    }
+  }
+
   killHoverThrusters() {
+    if (this.docking)
+       return;
     this.changingDirection();
     this.logState('Shutting off hover thrusters...');
     this.wasKillingThrusters = true;
     this.hoverThrusterRestoreTime = this.getNowPlus(1);
+    this.killInProgressVerticalThrust();
+  }
+
+  killInProgressVerticalDrop() {
+    var now = new Date();
+    if (this.hoverThrusterRestoreTime > now) {
+      this.hoverThrusterRestoreTime = now;
+      this.wasKillingThrusters = false;
+    }
   }
 
   fireMainThrusters() {
+    if (this.retractingEngines || this.enginesRetracted || this.docking)
+      return;
     this.changingDirection();
     this.logState('Firing main thrusters...');
     this.wasFiringMainThrusters = true;
     this.mainThrusterOfftime = this.getNowPlus(1);
+    this.killInProgressVerticalDrop();
   }
 
   fireLeftThruster() {
+    if (this.retractingEngines || this.enginesRetracted || this.docking)
+      return;
     this.changingDirection();
     this.logState('Firing left thruster...');
     this.wasFiringLeftThruster = true;
@@ -232,6 +349,8 @@ class Rocket {
   }
 
   fireRightThruster() {
+    if (this.retractingEngines || this.enginesRetracted || this.docking)
+      return;
     this.changingDirection();
     this.logState('Firing right thruster...');
     this.wasFiringRightThruster = true;
@@ -239,7 +358,6 @@ class Rocket {
   }
 
   changeVelocity(velocityX, velocityY) {
-    //console.log('changeVelocity: this.timeStart = new Date();');
     this.timeStart = new Date();
     this.velocityX = velocityX;
     this.velocityY = velocityY;
@@ -249,8 +367,13 @@ class Rocket {
   }
 
   changeVelocityBy(deltaVelocityX, deltaVelocityY) {
-    var newVelocityX = this.velocityX * deltaVelocityX;
-    var newVelocityY = this.velocityY * deltaVelocityY;
+    var now = new Date();
+    var secondsPassed = (now - this.timeStart) / 1000;
+    var velocityX = Physics.getFinalVelocity(secondsPassed, this.velocityX, this.getHorizontalAcceleration());
+    var velocityY = Physics.getFinalVelocity(secondsPassed, this.velocityY, this.getVerticalAcceleration());
+
+    var newVelocityX = velocityX * deltaVelocityX;
+    var newVelocityY = velocityY * deltaVelocityY;
 
     if (Math.abs(newVelocityX) < 0.0008)
       newVelocityX = 0;
@@ -281,6 +404,9 @@ class Rocket {
     this.x = this.startX + Physics.metersToPixels(xDisplacement);
 
     var yDisplacement = Physics.getDisplacement(secondsPassed, this.velocityY, vAccel);
+    if (this.chuteSailsAreFull && yDisplacement > ChuteMaxVelocity * secondsPassed)
+      yDisplacement = ChuteMaxVelocity * secondsPassed;
+
     this.y = this.startY + Physics.metersToPixels(yDisplacement);
 
     //console.log('Displacement: (' + xDisplacement + ', ' + yDisplacement + ')');
@@ -292,73 +418,140 @@ class Rocket {
     this.changeVelocity(this.lastVelocityX, this.lastVelocityY);
   }
 
+  launch() {
+    myRocket.isDocked = false;
+    myRocket.isDocking = false;
+    myRocket.dockedLeft = false;
+    myRocket.dockedTop = false;
+    myRocket.changeVelocity(2.4, 0);
+    myRocket.extendEngines();
+  }
+
   draw(context) {
+    if (this.drawing)
+      return;
+    this.drawing = true;
     var now = new Date();
+
+    var bottomLeftEngineRetractionOffsetX = 0
+    var bottomLeftEngineRetractionOffsetY = 0
+    var bottomRightEngineRetractionOffsetX = 0;
+    var bottomRightEngineRetractionOffsetY = 0;
+    var rightEngineRetractionOffsetX = 0;
+    var rightEngineRetractionOffsetY = 0;
+    var leftEngineRetractionOffsetX = 0;
+    var leftEngineRetractionOffsetY = 0;
+
+    if (this.retractingEngines || this.extendingEngines) {
+      var timeIn;
+      var totalTime;
+      var percentageIn;
+
+      if (this.extendingEngines) {
+        totalTime = this.extensionEndTime - this.extensionStartTime;
+        timeIn = now - this.extensionStartTime;
+        var percentageOut = timeIn / totalTime
+        if (percentageOut > 1) {
+          this.extendingEngines = false;
+          percentageOut = 1;
+          this.enginesExtended = true;
+          this.enginesRetracted = false;
+        }
+        percentageIn = 1 - percentageOut;
+      }
+      else {
+        timeIn = now - this.retractionStartTime;
+        totalTime = this.retractionEndTime - this.retractionStartTime;
+        percentageIn = timeIn / totalTime;
+        if (percentageIn > 1) {
+          this.retractingEngines = false;
+          percentageIn = 1;
+          this.enginesRetracted = true;
+          this.enginesExtended = false;
+        }
+      }
+      
+      
+      bottomLeftEngineRetractionOffsetX = BottomLeftEngineRetractionOffsetX * percentageIn;
+      bottomLeftEngineRetractionOffsetY = BottomLeftEngineRetractionOffsetY * percentageIn;
+
+      bottomRightEngineRetractionOffsetX = BottomRightEngineRetractionOffsetX * percentageIn;
+      bottomRightEngineRetractionOffsetY = BottomRightEngineRetractionOffsetY * percentageIn;
+
+      leftEngineRetractionOffsetX = LeftEngineRetractionOffsetX * percentageIn;
+      leftEngineRetractionOffsetY = LeftEngineRetractionOffsetY * percentageIn;
+
+      rightEngineRetractionOffsetX = RightEngineRetractionOffsetX * percentageIn;
+      rightEngineRetractionOffsetY = RightEngineRetractionOffsetY * percentageIn;
+    }
+
     var offsettingVerticalAcceleration = false;
     var verticalVelocityDecay = 1.0;
     var horizontalVelocityDecay = 1.0;
 
-    if (this.mainThrusterOfftime > now) {
-      this.bottomRightFlame.draw(context, this.x, this.y);
-      this.bottomLeftFlame.draw(context, this.x, this.y);
-      offsettingVerticalAcceleration = true;
-    }
-    else {
-      if (this.wasFiringMainThrusters) {
-        this.wasFiringMainThrusters = false;
-        this.logState('Main thrusters off...');
-        this.thrustersOff();
-      }
-      var hoverThrusterKillOffset = 0;
-      if (this.hoverThrusterRestoreTime > now) {
+    if (!this.enginesRetracted && !this.retractingEngines && !this.extendingEngines) {
+      if ((this.docking && !this.dockedTop) || this.mainThrusterOfftime > now) {
+        this.bottomRightFlame.draw(context, this.x, this.y);
+        this.bottomLeftFlame.draw(context, this.x, this.y);
         offsettingVerticalAcceleration = true;
-        hoverThrusterKillOffset = 15;
       }
-      else if (this.wasKillingThrusters) {
-        this.logState('Hover thrusters restored...');
-        this.wasKillingThrusters = false;
+      else {
+        if (this.wasFiringMainThrusters) {
+          this.wasFiringMainThrusters = false;
+          this.logState('Main thrusters off...');
+          this.thrustersOff();
+        }
+        var hoverThrusterKillOffset = 0;
+        if (this.hoverThrusterRestoreTime > now) {
+          offsettingVerticalAcceleration = true;
+          hoverThrusterKillOffset = 15;
+        }
+        else if (this.wasKillingThrusters) {
+          this.logState('Hover thrusters restored...');
+          this.wasKillingThrusters = false;
+          this.thrustersOff();
+        }
+
+        this.bottomRightMiniFlame.draw(context, this.x, this.y - hoverThrusterKillOffset);
+        this.bottomLeftMiniFlame.draw(context, this.x, this.y - hoverThrusterKillOffset);
+      }
+
+      if (!offsettingVerticalAcceleration && this.velocityY != 0) {
+        // Stabilize vertical velocity if only the hover thrusters are firing.
+        verticalVelocityDecay = 0.99;
+      }
+
+      var offsettingHorizontalAcceleration = false;
+
+      if ((this.docking && !this.dockedLeft) || this.rightThrusterOfftime > now) {
+        offsettingHorizontalAcceleration = true;
+        this.rightFlame.draw(context, this.x, this.y);
+      }
+      else if (this.wasFiringRightThruster) {
+        this.logState('Right thruster off...');
+        this.wasFiringRightThruster = false;
         this.thrustersOff();
       }
 
-      this.bottomRightMiniFlame.draw(context, this.x, this.y - hoverThrusterKillOffset);
-      this.bottomLeftMiniFlame.draw(context, this.x, this.y - hoverThrusterKillOffset);
-    }
-    if (!offsettingVerticalAcceleration && this.velocityY != 0) {
-      // Stabilize vertical velocity if only the hover thrusters are firing.
-      verticalVelocityDecay = 0.99;
-    }
+      if (this.leftThrusterOfftime > now) {
+        offsettingHorizontalAcceleration = true;
+        this.leftFlame.draw(context, this.x, this.y);
+      }
+      else if (this.wasFiringLeftThruster) {
+        this.logState('Left thruster off...');
+        this.wasFiringLeftThruster = false;
+        this.thrustersOff();
+      }
 
+      if (!offsettingHorizontalAcceleration && this.velocityX != 0) {
+        // Stabilize horizontal velocity if only the hover thrusters are firing.
+        horizontalVelocityDecay = 0.99;
+      }
 
-
-    var offsettingHorizontalAcceleration = false;
-
-    if (this.rightThrusterOfftime > now) {
-      offsettingHorizontalAcceleration = true;
-      this.rightFlame.draw(context, this.x, this.y);
+      if (horizontalVelocityDecay != 1 || verticalVelocityDecay != 1) {
+        this.changeVelocityBy(horizontalVelocityDecay, verticalVelocityDecay);
+      }
     }
-    else if (this.wasFiringRightThruster) {
-      this.logState('Right thruster off...');
-      this.wasFiringRightThruster = false;
-      this.thrustersOff();
-    }
-
-    if (this.leftThrusterOfftime > now) {
-      offsettingHorizontalAcceleration = true;
-      this.leftFlame.draw(context, this.x, this.y);
-    }
-    else if (this.wasFiringLeftThruster) {
-      this.logState('Left thruster off...');
-      this.wasFiringLeftThruster = false;
-      this.thrustersOff();
-    }
-
-    if (!offsettingHorizontalAcceleration && this.velocityX != 0) {
-      // Stabilize horizontal velocity if only the hover thrusters are firing.
-      horizontalVelocityDecay = 0.99;
-    }
-
-    if (horizontalVelocityDecay != 1 || verticalVelocityDecay != 1)
-      this.changeVelocityBy(horizontalVelocityDecay, verticalVelocityDecay);
 
     var secondsPassed = (now - this.timeStart) / 1000;
     this.lastVelocityX = Physics.getFinalVelocity(secondsPassed, this.velocityX, this.getHorizontalAcceleration());
@@ -367,10 +560,12 @@ class Rocket {
     this.lastY = this.y;
 
 
-    this.leftEngine.draw(context, this.x, this.y);
-    this.rightEngine.draw(context, this.x, this.y);
-    this.bottomLeftEngine.draw(context, this.x, this.y);
-    this.bottomRightEngine.draw(context, this.x, this.y);
+    if (!this.enginesRetracted || this.extendingEngines) {
+      this.leftEngine.draw(context, this.x + leftEngineRetractionOffsetX, this.y + leftEngineRetractionOffsetY);
+      this.rightEngine.draw(context, this.x + rightEngineRetractionOffsetX, this.y + rightEngineRetractionOffsetY);
+      this.bottomLeftEngine.draw(context, this.x + bottomLeftEngineRetractionOffsetX, this.y + bottomLeftEngineRetractionOffsetY);
+      this.bottomRightEngine.draw(context, this.x + bottomRightEngineRetractionOffsetX, this.y + bottomRightEngineRetractionOffsetY);
+    }
 
     if (this.chuteDeployed) {
       this.chute.draw(context, this.x, this.y);
@@ -389,6 +584,7 @@ class Rocket {
     }
 
     this.codeRushedBody.draw(context, this.x, this.y);
+    this.drawing = false;
   }
 
   logState(message) {
